7d1abba447de6b5bbefa479633a8d90ab0c02026
energy1 <vsenetak88@mail.ru>
2017-11-10 18:47:42 +0200

    formatted to 2spaces

--- client/app/account/logout/logout.component.js
+++ client/app/account/logout/logout.component.js
@@ -1,30 +1,30 @@
 const logoutComponent = {
-	template: '',
-	controller: class logoutController {
-		constructor($state, $http, Auth, CartService) {
-			'ngInject';
-			this.Auth = Auth;
-			this.$http = $http;
-			this.$state = $state;
-			this.loadCart = CartService.loadCart.bind(CartService);
+  template: '',
+  controller: class logoutController {
+    constructor($state, $http, Auth, CartService) {
+      'ngInject';
+      this.Auth = Auth;
+      this.$http = $http;
+      this.$state = $state;
+      this.loadCart = CartService.loadCart.bind(CartService);
 
-			this.Auth.logout();
-			this.logoutSession();
-		}
+      this.Auth.logout();
+      this.logoutSession();
+    }
 
-		logoutSession() {
-			return this.$http.get('/auth/local/logout')
-				.then(() => {
-					this.loadCart();
+    logoutSession() {
+      return this.$http.get('/auth/local/logout')
+        .then(() => {
+          this.loadCart();
 
-					this.$state.go('main.home');
-				})
-				.catch((error) => {
-					console.error(error);
-					this.$state.go('404');
-				});
-		}
-	}
+          this.$state.go('main.home');
+        })
+        .catch((error) => {
+          console.error(error);
+          this.$state.go('404');
+        });
+    }
+  }
 };
 
 export default logoutComponent;
